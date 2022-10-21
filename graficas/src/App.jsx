94a3e874ab6386e301efd0497f3aafa346d261f9
arturobl00 <arturobl00@msn.com>
2022-10-21 09:38:41 -0500

    Graficas

---
+++ graficas/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LineChart from "./LineChart";
+
+function App() {
+  return (
+    <div className="App" style={{width: '100', height: '500px'}}>
+      <h1>Trabajando Graficas en React</h1>
+      <LineChart/>
+    </div>
+  );
+}
+
+export default App;
